66004ff1e4efbaa6693f9f4f5cf68379eacff593
info1bigfeatherstech-ship-it <info1.bigfeatherstech@gmail.com>
2026-04-10 09:19:46 +0530

    with backup plan for frontend url

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL ? [process.env.FRONTEND_URL] : ['http://localhost:5173', 'http://127.0.0.1:5500', 'http://localhost:5500'],
+  origin: process.env.FRONTEND_URL ? [process.env.FRONTEND_URL] : ['http://localhost:5173', 'http://127.0.0.1:5500', 'http://localhost:5500' , 'https://offerwaalebaba.netlify.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
